c19b9bf6bbe24435b2a97e68f75eeb83ce79287c
Thierry Chatel <tchatel@gmail.com>
2013-11-01 23:47:25 +0100

    point de départ - étape 1

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 "use strict";
 
-var app = angular.module('app', []);
+angular.module('app', [])
 
-app.controller('Ctrl', function($scope) {
-	$scope.maps = [
-		{ center: {lat: 47.211, lng: -1.566}, zoom: 12 },
-		{ center: {lat: 37.423, lng: -122.086}, zoom: 9 }
-	];
-});
+    .controller('Ctrl', function($scope) {
+        $scope.maps = [
+            { center: {lat: 47.211, lng: -1.566}, zoom: 12 },
+            { center: {lat: 37.423, lng: -122.086}, zoom: 9 }
+        ];
+    })
 
-app.directive('gmaps', function factory() {
-    return {
-	    restrict: 'A',
-	    scope: false,
-	    link: function postLink(scope, element, attrs) {
+    .directive('gmaps', function factory() {
+        return {
+            restrict: 'A',
+            scope: false,
+            link: function postLink(scope, element, attrs) {
 
-            // TODO
+                // TODO
 
-        }
-    };
-});
+            }
+        };
+    });
